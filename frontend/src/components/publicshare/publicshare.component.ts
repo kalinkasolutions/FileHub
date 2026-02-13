401d1338dc0436c8dc60e422698e210d8c507335
kalinka <kalinka@kalinka.ch>
2026-02-13 22:55:23 +0100

    feat: add download link

--- frontend/src/components/publicshare/publicshare.component.ts
+++ frontend/src/components/publicshare/publicshare.component.ts
@@ -1,42 +1,72 @@
-import { CommonModule } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
-import { Meta, Title } from "@angular/platform-browser";
-import { ActivatedRoute } from "@angular/router";
-import { IShare } from "@models/IShare";
-import { FileService } from "@services/file.service";
-import { ShareService } from "@services/share.service";
-import { FileSize } from "util/filesize";
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from '@env/environment';
+import { NotificationLevel } from '@models/INotifcation';
+import { IShare } from '@models/IShare';
+import { FileService } from '@services/file.service';
+import { NotificationService } from '@services/notification.service';
+import { ShareService } from '@services/share.service';
+import { FileSize } from 'util/filesize';
 
 @Component({
-    standalone: true,
-    selector: 'app-publicshare',
-    templateUrl: './publicshare.component.html',
-    styleUrl: './publicshare.component.scss',
-    imports: [CommonModule]
+  standalone: true,
+  selector: 'app-publicshare',
+  templateUrl: './publicshare.component.html',
+  styleUrl: './publicshare.component.scss',
+  imports: [CommonModule],
 })
 export class PublicShare {
+  public share: IShare = {
+    Id: '',
+    Size: 0,
+    Name: '',
+    IsDir: false,
+  };
 
-    public share: IShare = {
-        Id: "",
-        Size: 0,
-        Name: "",
-        IsDir: false
-    }
+  constructor(
+    private route: ActivatedRoute,
+    private fileService: FileService,
+    private shareService: ShareService,
+    private notificationService: NotificationService,
+  ) {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id') ?? '';
+      this.shareService.validateShare(id).subscribe((share) => {
+        this.share = share;
+      });
+    });
+  }
 
-    constructor(private route: ActivatedRoute, private fileService: FileService, private shareService: ShareService) {
-        this.route.paramMap.subscribe(params => {
-            const id = params.get('id') ?? "";
-            this.shareService.validateShare(id).subscribe(share => {
-                this.share = share;
-            });
+  public download() {
+    this.fileService.downloadPublicShare(this.share);
+  }
+
+  public get Link() {
+    return `${environment.apiUrl}/public-api/files/download/${this.share.Id}`;
+  }
+
+  public copyLink(e: Event) {
+    e.preventDefault();
+    navigator.clipboard
+      .writeText(this.Link)
+      .then(() => {
+        this.notificationService.notify({
+          level: NotificationLevel.success,
+          title: 'Direct link',
+          message: 'Copied to clipboard',
         });
-    }
+      })
+      .catch((err) => {
+        this.notificationService.notify({
+          level: NotificationLevel.success,
+          title: 'Direct link',
+          message: `Failed to copy to clipboard ${err.message}`,
+        });
+      });
+  }
 
-    public download() {
-        this.fileService.downloadPublicShare(this.share);
-    }
-
-    public getFileSize(size: number): string {
-        return FileSize.FileSize(size);
-    }
+  public getFileSize(size: number): string {
+    return FileSize.FileSize(size);
+  }
 }
--- frontend/src/services/notification.service.ts
+++ frontend/src/services/notification.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from "@angular/core";
-import { INotification, NotificationLevel } from "@models/INotifcation";
-import { Subject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { INotification, NotificationLevel } from '@models/INotifcation';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class NotificationService {
-    private notificationSubject = new Subject<INotification>();
+  private notificationSubject = new Subject<INotification>();
 
-    public notifcations$ = this.notificationSubject.asObservable();
+  public notifcations$ = this.notificationSubject.asObservable();
 
-    public notify(notification: INotification) {
-        this.notificationSubject.next(notification);
-    }
+  public notify(notification: INotification) {
+    this.notificationSubject.next(notification);
+  }
 }
